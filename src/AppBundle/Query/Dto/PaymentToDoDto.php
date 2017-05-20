d779f36cda45540f081b9c7e04aac40366b90135
Adrian Pietka <adrian@pietka.me>
2017-05-20 19:18:38 +0200

    Fixed properties

--- src/AppBundle/Query/Dto/PaymentToDoDto.php
+++ src/AppBundle/Query/Dto/PaymentToDoDto.php
@@ -13,7 +13,7 @@ class PaymentToDoDto implements \JsonSerializable
     public function __construct(array $data)
     {
         $this->id = isset($data['id']) ? (int)$data['id'] : null;
-        $this->added = isset($data['added']) ? new \DateTime($added) : null;
+        $this->added = isset($data['added']) ? (new \DateTime($data['added']))->format('Y-m-d H:i:s') : null;
         $this->name = isset($data['name']) ? (string)$data['name'] : '';
         $this->amount = isset($data['amount']) ? (int)$data['amount'] : 0;
         $this->currency = isset($data['currency']) ? (string)$data['currency'] : '';
