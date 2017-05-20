0f74b150f8f6e7a50763a6a782a38eea371b3e52
Adrian Pietka <adrian@pietka.me>
2017-05-20 21:34:48 +0200

    Condition to query

--- src/AppBundle/Query/GetPaymentsToDoQuery.php
+++ src/AppBundle/Query/GetPaymentsToDoQuery.php
@@ -5,6 +5,8 @@ namespace AppBundle\Query;
 use AppBundle\Query\Dto\PaymentsToDoCollectionDto;
 use CqrsBundle\Querying\QueryInterface;
 use Doctrine\DBAL\Connection as Dbal;
+use PayAssistantBundle\Entity\Action;
+use PayAssistantBundle\Entity\User;
 
 class GetPaymentsToDoQuery implements QueryInterface
 {
@@ -20,9 +22,12 @@ class GetPaymentsToDoQuery implements QueryInterface
         $sql = 'SELECT a.id, a.added, d.name, a.value AS amount, "PLN" AS currency
                 FROM `action` a
                 JOIN definition d ON (d.id = a.definition_id)
-                WHERE d.user_id = :user_id';
-        $tips = $dbal->fetchAll($sql, [':user_id' => $this->userId]);
+                WHERE d.user_id = :user_id AND a.status = :status';
+        $payments = $dbal->fetchAll($sql, [
+            ':user_id' => $this->userId,
+            ':status' => Action::STATUS_TODO
+        ]);
 
-        return new PaymentsToDoCollectionDto($tips);
+        return new PaymentsToDoCollectionDto($payments);
     }
 }
